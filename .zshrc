ebffc54a4de0d4e530a4660fce0844e5e3de92e7
Nema Darban <nema.darban@gmail.com>
2018-05-01 11:52:15 -0700

    switching over to powerline9k

--- .zshrc
+++ .zshrc
@@ -5,7 +5,7 @@ export ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="agnoster"
+ZSH_THEME="powerlevel9k/powerlevel9k"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
@@ -72,9 +72,9 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 # ssh
 # export SSH_KEY_PATH="~/.ssh/dsa_id"
 
-DEFAULT_USER=nemadarban
+DEFAULT_USER=$USER
 
-source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+#source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
--- Mac_Setup.sh
+++ Mac_Setup.sh
@@ -48,11 +48,14 @@ esac
 read -r -p "Would you like to setup your Terminal now? [Y/N]" response
 case $response in
     [yY][eE][sS]|[yY])
-        echo "install powerline"
-        cd $HOME/Mac-Settings
-	git clone git@github.com:powerline/powerline.git
-	rsync -av powerline/powerline /usr/local/lib/python2.7/site-packages/
-	rsync -av powerline/scripts /usr/local/lib/python2.7/site-packages/
+    echo "install powerline"
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    echo "Installing PowerLevel9k"
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    cd $HOME/Mac-Settings
+	# git clone git@github.com:powerline/powerline.git
+	# rsync -av powerline/powerline /usr/local/lib/python2.7/site-packages/
+	# rsync -av powerline/scripts /usr/local/lib/python2.7/site-packages/
         clear
         git clone https://github.com/smalldoorman/Mac-Settings.git $HOME/Mac-Settings
         cd $HOME/Mac-Settings
@@ -62,6 +65,7 @@ case $response in
         open Solarized\ Dark.terminal
         sleep 2
         read -r -p "Please make 'Solarized Dark.terminal' the default profile then press Enter to continue..." key
+        read -r -p "Please make 'Inconsolata' the default font then press Enter to continue..." key
 
         ;;
     *)
